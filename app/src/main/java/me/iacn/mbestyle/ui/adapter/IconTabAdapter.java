0400f5be820a7dcc6a201293cd3890da6868a34e
iAcn <iacnlyx@gmail.com>
2017-02-18 16:28:44 +0800

    IconFragment：添加 Tab 和 ViewPager

---
+++ app/src/main/java/me/iacn/mbestyle/ui/adapter/IconTabAdapter.java
@@ -0,0 +1,27 @@
+package me.iacn.mbestyle.ui.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by iAcn on 2017/2/18
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class IconTabAdapter extends FragmentPagerAdapter {
+
+    public IconTabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
