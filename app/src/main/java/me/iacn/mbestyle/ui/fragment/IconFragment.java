73bfb641458b6ff7510d576a4a41e2589ad0bade
iAcn <iacnlyx@gmail.com>
2017-02-18 15:22:29 +0800

    IconFragment：建立

---
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/IconFragment.java
@@ -0,0 +1,31 @@
+package me.iacn.mbestyle.ui.fragment;
+
+import me.iacn.mbestyle.R;
+
+/**
+ * Created by iAcn on 2017/2/18
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class IconFragment extends BaseFragment {
+
+    @Override
+    protected int getInflateView() {
+        return R.layout.fragment_icon;
+    }
+
+    @Override
+    protected void findView() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
