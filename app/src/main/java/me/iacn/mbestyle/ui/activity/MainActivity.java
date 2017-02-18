249888345cfd1c154d928a12c403389fe162ac9f
iAcn <iacnlyx@gmail.com>
2017-02-18 13:32:54 +0800

    MainActivity：创建

---
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/MainActivity.java
@@ -0,0 +1,12 @@
+package me.iacn.mbestyle.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by iAcn on 2017/2/18
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+}
