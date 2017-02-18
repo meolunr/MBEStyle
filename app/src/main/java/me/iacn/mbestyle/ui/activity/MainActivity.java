e3521c078c9178ff24c526f34bf19be35aab717f
meolunr <meolunr@gmail.com>
2017-02-18 14:59:21 +0800

    MainActivity：主界面添加

--- app/src/main/java/me/iacn/mbestyle/ui/activity/MainActivity.java
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package me.iacn.mbestyle.ui.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import me.iacn.mbestyle.R;
 
 /**
  * Created by iAcn on 2017/2/18
@@ -9,4 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
