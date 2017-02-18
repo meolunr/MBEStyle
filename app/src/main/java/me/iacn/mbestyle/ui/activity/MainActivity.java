84a16f8215a77e41a149c9cf2e0898abc25defc8
iAcn <iacnlyx@gmail.com>
2017-02-18 15:28:05 +0800

    MainActivity：Bottom 点击

--- app/src/main/java/me/iacn/mbestyle/ui/activity/MainActivity.java
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package me.iacn.mbestyle.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import me.iacn.mbestyle.R;
 import me.iacn.mbestyle.util.StatusBarUtils;
@@ -13,7 +16,7 @@ import me.iacn.mbestyle.util.StatusBarUtils;
  * Emali iAcn0301@foxmail.com
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,5 +24,28 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         StatusBarUtils.setColor(this, ContextCompat.getColor(this, R.color.colorPrimary));
+
+        BottomNavigationView bottomView = (BottomNavigationView) findViewById(R.id.bottom_bar);
+        bottomView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_icon:
+                return true;
+
+            case R.id.menu_launcher:
+                return true;
+
+            case R.id.menu_apply:
+                return true;
+
+            case R.id.menu_about:
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
