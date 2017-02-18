a6cbc88f70be1f90fed54d71f5f5104685273129
iAcn <iacnlyx@gmail.com>
2017-02-18 20:53:01 +0800

    AboutItem：应用attrs 属性

--- app/src/main/java/me/iacn/mbestyle/ui/widget/AboutItem.java
+++ app/src/main/java/me/iacn/mbestyle/ui/widget/AboutItem.java
@@ -29,22 +29,25 @@ public class AboutItem extends FrameLayout {
 
     public AboutItem(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initView(attrs);
-
-        super(context, attrs, defStyleAttr);
         initView(context);
 
-        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.IconButton);
-
-        ivIcon.setImageResource(array.getResourceId(R.styleable.IconButton_btn_icon, 0));
-        tvName.setText(array.getString(R.styleable.IconButton_btn_name));
-
-        array.recycle();
+        if (attrs != null) {
+            setAttr(context, attrs);
+        }
     }
 
     private void initView(Context context) {
         LayoutInflater.from(context).inflate(R.layout.inflate_about_item, this, true);
-        ivIcon = (ImageView) findViewById(R.id.iv_icon);
-        tvName = (TextView) findViewById(R.id.tv_name);
+        tvTitle = (TextView) findViewById(R.id.tv_title);
+        tvSummary = (TextView) findViewById(R.id.tv_summary);
+    }
+
+    private void setAttr(Context context, AttributeSet attrs) {
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.AboutItem);
+
+        tvTitle.setText(array.getString(R.styleable.AboutItem_title));
+        tvSummary.setText(array.getString(R.styleable.AboutItem_summary));
+
+        array.recycle();
     }
 }
