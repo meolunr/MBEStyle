0b991529560942ed8bacccec705b702fef5406cc
meolunr <meolunr@gmail.com>
2017-02-18 15:19:01 +0800

    BaseFragment：建立

---
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/BaseFragment.java
@@ -0,0 +1,46 @@
+package me.iacn.mbestyle.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by iAcn on 2017/2/18
+ * Emali iAcn0301@foxmail.com
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return View.inflate(getActivity(), getInflateView(), null);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        findView();
+        setListener();
+        initData();
+    }
+
+    @LayoutRes
+    protected abstract int getInflateView();
+
+    protected abstract void findView();
+
+    protected abstract void setListener();
+
+    protected abstract void initData();
+
+    protected final View findViewById(@IdRes int id) {
+        return getView().findViewById(id);
+    }
+}
