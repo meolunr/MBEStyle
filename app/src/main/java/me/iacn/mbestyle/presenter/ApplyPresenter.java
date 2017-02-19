feb5dda31fba39c95c029dd78c1b365b7cd8a63f
meolunr <meolunr@gmail.com>
2017-02-19 18:31:02 +0800

    PackageUtils：通过 Main Intent 获得 App

--- app/src/main/java/me/iacn/mbestyle/presenter/ApplyPresenter.java
+++ app/src/main/java/me/iacn/mbestyle/presenter/ApplyPresenter.java
@@ -1,6 +1,15 @@
 package me.iacn.mbestyle.presenter;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+import me.iacn.mbestyle.bean.AppBean;
 import me.iacn.mbestyle.ui.fragment.ApplyFragment;
+import me.iacn.mbestyle.util.PackageUtils;
 
 /**
  * Created by iAcn on 2017/2/19
--- app/src/main/java/me/iacn/mbestyle/util/PackageUtils.java
+++ app/src/main/java/me/iacn/mbestyle/util/PackageUtils.java
@@ -1,8 +1,10 @@
 package me.iacn.mbestyle.util;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
 import java.util.List;
 
@@ -16,5 +18,14 @@ public class PackageUtils {
     public static List<ApplicationInfo> getAllApp(Context context) {
         PackageManager manager = context.getPackageManager();
         return manager.getInstalledApplications(PackageManager.COMPONENT_ENABLED_STATE_DEFAULT);
+    }
+
+    public static List<ResolveInfo> getAppByMainIntent(Context context) {
+        PackageManager manager = context.getPackageManager();
+
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        return manager.queryIntentActivities(intent, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT);
     }
 }
