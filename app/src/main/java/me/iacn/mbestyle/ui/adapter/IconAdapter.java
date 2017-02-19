36acf4577d1c482e8f88bea52f324b601e2257bc
iAcn <iacnlyx@gmail.com>
2017-02-19 15:08:38 +0800

    IconAllFragment：显示所有图标

--- app/src/main/java/me/iacn/mbestyle/ui/adapter/IconAdapter.java
+++ app/src/main/java/me/iacn/mbestyle/ui/adapter/IconAdapter.java
@@ -4,8 +4,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
 
 import me.iacn.mbestyle.R;
+import me.iacn.mbestyle.bean.IconBean;
 
 /**
  * Created by iAcn on 2017/2/18
@@ -13,6 +17,12 @@ import me.iacn.mbestyle.R;
  */
 
 public class IconAdapter extends RecyclerView.Adapter<IconHolder> {
+
+    private List<IconBean> mIcons;
+
+    public IconAdapter(List<IconBean> mIcons) {
+        this.mIcons = mIcons;
+    }
 
     @Override
     public IconHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -22,18 +32,22 @@ public class IconAdapter extends RecyclerView.Adapter<IconHolder> {
 
     @Override
     public void onBindViewHolder(IconHolder holder, int position) {
-
+        IconBean bean = mIcons.get(position);
+        holder.ivIcon.setImageResource(bean.id);
     }
 
     @Override
     public int getItemCount() {
-        return 40;
+        return mIcons.size();
     }
 }
 
 class IconHolder extends RecyclerView.ViewHolder {
 
+    ImageView ivIcon;
+
     IconHolder(View itemView) {
         super(itemView);
+        ivIcon = (ImageView) itemView.findViewById(R.id.iv_icon);
     }
 }
