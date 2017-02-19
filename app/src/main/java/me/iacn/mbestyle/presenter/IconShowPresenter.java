39b99843094654a55b5d9394c8c6132a3dded2c7
iAcn <iacnlyx@gmail.com>
2017-02-19 17:13:58 +0800

    IconAdaptedFragment：显示已安装的应用图标

--- app/src/main/java/me/iacn/mbestyle/presenter/IconShowPresenter.java
+++ app/src/main/java/me/iacn/mbestyle/presenter/IconShowPresenter.java
@@ -1,11 +1,14 @@
 package me.iacn.mbestyle.presenter;
 
+import android.content.pm.ApplicationInfo;
 import android.content.res.XmlResourceParser;
 
 import org.xmlpull.v1.XmlPullParser;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
@@ -14,11 +17,13 @@ import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import me.iacn.mbestyle.BuildConfig;
 import me.iacn.mbestyle.R;
 import me.iacn.mbestyle.bean.IconBean;
 import me.iacn.mbestyle.ui.fragment.BaseIconFragment;
+import me.iacn.mbestyle.util.PackageUtils;
 
 /**
  * Created by iAcn on 2017/2/18
@@ -92,18 +97,32 @@ public class IconShowPresenter {
                             icons.add(bean);
                         }
                     }
-
                     xml.next();
                 }
 
-                System.out.println(icons);
                 flowableEmitter.onNext(icons);
             }
         }, BackpressureStrategy.BUFFER)
+                .map(new Function<List<IconBean>, List<IconBean>>() {
+                    @Override
+                    public List<IconBean> apply(@NonNull List<IconBean> list) throws Exception {
+                        List<IconBean> newList = new ArrayList<>();
+                        Set<String> appPkgNames = getAppPkgNames();
+
+                        for (IconBean bean : list) {
+                            if (appPkgNames.contains(bean.iconPkgName)) {
+                                newList.add(bean);
+                            }
+                        }
+
+                        return newList;
+                    }
+                })
                 .subscribe(new Consumer<List<IconBean>>() {
                     @Override
                     public void accept(@NonNull List<IconBean> list) throws Exception {
-
+                        System.out.println(list);
+                        mView.showIcons(list);
                     }
                 });
     }
@@ -115,5 +134,16 @@ public class IconShowPresenter {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private Set<String> getAppPkgNames() {
+        List<ApplicationInfo> apps = PackageUtils.getAllApp(mView.getActivity());
+        Set<String> set = new HashSet<>();
+
+        for (ApplicationInfo info : apps) {
+            set.add(info.packageName);
+        }
+
+        return set;
     }
 }
