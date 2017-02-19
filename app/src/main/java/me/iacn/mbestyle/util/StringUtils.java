3422b1c145df944efbe9b89a3516d472603704aa
meolunr <meolunr@gmail.com>
2017-02-19 20:50:16 +0800

    ApplyFragment：复制应用信息到剪贴板

---
+++ app/src/main/java/me/iacn/mbestyle/util/StringUtils.java
@@ -0,0 +1,17 @@
+package me.iacn.mbestyle.util;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+
+/**
+ * Created by iAcn on 2016/8/17
+ * Emali iAcn0301@foxmail.com
+ */
+public class StringUtils {
+
+    public static void copyToClipboard(Context context, String str) {
+        ClipboardManager manager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        manager.setPrimaryClip(ClipData.newPlainText(null, str));
+    }
+}
