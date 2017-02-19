3675d8bf8d600005ac34810a5fa143c9cb034256
meolunr <meolunr@gmail.com>
2017-02-19 20:27:19 +0800

    ApplyFragment：列表点击事件处理

---
+++ app/src/main/java/me/iacn/mbestyle/ui/callback/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package me.iacn.mbestyle.ui.callback;
+
+import android.view.View;
+
+/**
+ * Created by iAcn on 2017/2/19
+ * Emali iAcn0301@foxmail.com
+ */
+
+public interface OnItemClickListener {
+
+    void onItemClick(View itemView, int position);
+}
--- app/src/main/java/me/iacn/mbestyle/ui/fragment/ApplyFragment.java
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/ApplyFragment.java
@@ -2,6 +2,7 @@ package me.iacn.mbestyle.ui.fragment;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.List;
 
@@ -9,13 +10,14 @@ import me.iacn.mbestyle.R;
 import me.iacn.mbestyle.bean.AppBean;
 import me.iacn.mbestyle.presenter.ApplyPresenter;
 import me.iacn.mbestyle.ui.adapter.AppAdapter;
+import me.iacn.mbestyle.ui.callback.OnItemClickListener;
 
 /**
  * Created by iAcn on 2017/2/18
  * Emali iAcn0301@foxmail.com
  */
 
-public class ApplyFragment extends BaseFragment {
+public class ApplyFragment extends BaseFragment implements OnItemClickListener {
 
     private RecyclerView rvApp;
     private ApplyPresenter mPresenter;
@@ -43,6 +45,12 @@ public class ApplyFragment extends BaseFragment {
 
     public void showApps(List<AppBean> list) {
         AppAdapter adapter = new AppAdapter(list);
+        adapter.setOnItemClickListener(this);
         rvApp.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View itemView, int position) {
+        System.out.println(itemView);
     }
 }
