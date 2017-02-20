8063540ad3ad68855cb601c612179be1293326bc
iAcn <iacnlyx@gmail.com>
2017-02-20 20:47:00 +0800

    ApplyFragment：异步加载动画

--- app/src/main/java/me/iacn/mbestyle/ui/fragment/ApplyFragment2.java
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/ApplyFragment2.java
@@ -3,6 +3,7 @@ package me.iacn.mbestyle.ui.fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -11,20 +12,21 @@ import me.iacn.mbestyle.bean.AppBean;
 import me.iacn.mbestyle.presenter.ApplyPresenter;
 import me.iacn.mbestyle.ui.adapter.AppAdapter;
 import me.iacn.mbestyle.ui.callback.OnItemClickListener;
+import me.iacn.mbestyle.util.StringUtils;
 
 /**
  * Created by iAcn on 2017/2/18
  * Emali iAcn0301@foxmail.com
  */
 
-public class ApplyFragment extends ILazyFragment implements OnItemClickListener {
+public class ApplyFragment2 extends BaseFragment implements OnItemClickListener {
 
     private RecyclerView rvApp;
     private ApplyPresenter mPresenter;
     private List<AppBean> mApps;
 
     @Override
-    protected int getContentView() {
+    protected int getInflateView() {
         return R.layout.fragment_apply;
     }
 
@@ -40,21 +42,36 @@ public class ApplyFragment extends ILazyFragment implements OnItemClickListener 
 
     @Override
     protected void initData() {
-        mPresenter = new ApplyPresenter(this);
+//        mPresenter = new ApplyPresenter(this);
         mPresenter.loadInstallApp();
     }
 
-    @Override
-    public void onItemClick(View itemView, int position) {
-
-    }
-
-    public void onLoadData(List<AppBean> list) {
-        super.onLoadData();
-
+    public void showApps(List<AppBean> list) {
         mApps = list;
         AppAdapter adapter = new AppAdapter(mApps);
         adapter.setOnItemClickListener(this);
         rvApp.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View itemView, int position) {
+        AppBean bean = mApps.get(position);
+        String template = getString(R.string.component_template);
+        String[] split = bean.activity.split("/");
+
+        if (split[1].startsWith(".")) {
+            // 正常应用
+            template = template
+                    .replace("$packageName$", split[0])
+                    .replace("$activityName$", split[0] + split[1]);
+        } else {
+            // 蛋疼应用
+            template = template
+                    .replace("$packageName$", split[0])
+                    .replace("$activityName$", split[1]);
+        }
+
+        StringUtils.copyToClipboard(getActivity(), template);
+        Toast.makeText(getActivity(), "应用信息已复制到剪贴板", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/me/iacn/mbestyle/ui/fragment/ILazyFragment.java
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/ILazyFragment.java
@@ -8,31 +8,46 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import me.iacn.mbestyle.R;
 
 /**
- * Created by iAcn on 2017/2/18
+ * Created by iAcn on 2017/2/20
  * Emali iAcn0301@foxmail.com
  */
 
-public abstract class BaseFragment extends Fragment {
+public abstract class ILazyFragment extends Fragment {
+
+    protected View mContentView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return View.inflate(getActivity(), getInflateView(), null);
+        return View.inflate(getActivity(), getInitialView(), null);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        setContentView();
         findView();
         setListener();
         initData();
     }
 
     @LayoutRes
-    protected abstract int getInflateView();
+    protected int getInitialView() {
+        return R.layout.inflate_loading;
+    }
+
+    protected final void setContentView() {
+        mContentView = View.inflate(getActivity(), getContentView(), null);
+    }
+
+    @LayoutRes
+    protected abstract int getContentView();
 
     protected abstract void findView();
 
@@ -41,6 +56,15 @@ public abstract class BaseFragment extends Fragment {
     protected abstract void initData();
 
     protected final View findViewById(@IdRes int id) {
-        return getView().findViewById(id);
+        return mContentView.findViewById(id);
+    }
+
+    protected void onLoadData() {
+        FrameLayout layout = (FrameLayout) getView();
+
+        if (layout != null) {
+            layout.removeAllViews();
+            layout.addView(mContentView);
+        }
     }
 }
