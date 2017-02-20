1c6f76117743e6afffcd4b044dd6d3b1e8f5fe05
iAcn <iacnlyx@gmail.com>
2017-02-20 21:02:08 +0800

    ILazyFragment：减少一层布局层级

--- app/src/main/java/me/iacn/mbestyle/ui/fragment/BaseFragment.java
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/BaseFragment.java
@@ -19,7 +19,7 @@ public abstract class BaseFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return View.inflate(getActivity(), getContentView(), null);
+        return View.inflate(getActivity(), getInflateView(), null);
     }
 
     @Override
@@ -32,7 +32,7 @@ public abstract class BaseFragment extends Fragment {
     }
 
     @LayoutRes
-    protected abstract int getContentView();
+    protected abstract int getInflateView();
 
     protected abstract void findView();
 
