27eb591edc224fe7c4603f28937fd1815cebf310
iAcn <iacnlyx@gmail.com>
2017-02-20 11:15:07 +0800

    LauncherFragment：添加  RecyclerView

--- app/src/main/java/me/iacn/mbestyle/ui/fragment/LauncherFragment.java
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/LauncherFragment.java
@@ -1,5 +1,8 @@
 package me.iacn.mbestyle.ui.fragment;
 
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import me.iacn.mbestyle.R;
 
 /**
@@ -9,20 +12,21 @@ import me.iacn.mbestyle.R;
 
 public class LauncherFragment extends BaseFragment {
 
+    private RecyclerView rvLauncher;
+
     @Override
     protected int getInflateView() {
-        System.out.println("LauncherFragment");
         return R.layout.fragment_launcher;
     }
 
     @Override
     protected void findView() {
-
+        rvLauncher = (RecyclerView) findViewById(R.id.rv_launcher);
     }
 
     @Override
     protected void setListener() {
-
+        rvLauncher.setLayoutManager(new GridLayoutManager(getActivity(), 2));
     }
 
     @Override
