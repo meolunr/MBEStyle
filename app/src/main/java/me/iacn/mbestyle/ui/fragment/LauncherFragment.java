82a894cbc68fed8066781ff90c459d9fd74e02b7
meolunr <meolunr@gmail.com>
2017-02-20 12:03:22 +0800

    LauncherFragment：应用到 Nova

--- app/src/main/java/me/iacn/mbestyle/ui/fragment/LauncherFragment.java
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/LauncherFragment.java
@@ -1,9 +1,11 @@
 package me.iacn.mbestyle.ui.fragment;
 
+import android.content.Intent;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import me.iacn.mbestyle.BuildConfig;
 import me.iacn.mbestyle.R;
 import me.iacn.mbestyle.ui.adapter.LauncherAdapter;
 import me.iacn.mbestyle.ui.callback.OnItemClickListener;
@@ -42,6 +44,19 @@ public class LauncherFragment extends BaseFragment implements OnItemClickListene
 
     @Override
     public void onItemClick(View itemView, int position) {
-        System.out.println(position);
+        switch (position) {
+            case 0:
+                applyToNovaLauncher();
+                break;
+        }
+    }
+
+    private void applyToNovaLauncher() {
+        Intent intent = new Intent("com.teslacoilsw.launcher.APPLY_ICON_THEME");
+        intent.setPackage("com.teslacoilsw.launcher");
+        intent.putExtra("com.teslacoilsw.launcher.extra.ICON_THEME_TYPE", "GO");
+        intent.putExtra("com.teslacoilsw.launcher.extra.ICON_THEME_PACKAGE", BuildConfig.APPLICATION_ID);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
