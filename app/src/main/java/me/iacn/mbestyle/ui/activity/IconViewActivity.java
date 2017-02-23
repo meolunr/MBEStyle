720edc926a60923f6b685bb83a14cd7707b9f7f3
iAcn <iacnlyx@gmail.com>
2017-02-23 17:18:52 +0800

    IconViewActivity：创建

---
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/IconViewActivity.java
@@ -0,0 +1,21 @@
+package me.iacn.mbestyle.ui.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.iacn.mbestyle.R;
+
+/**
+ * Created by iAcn on 2017/2/23
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class IconViewActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_icon_view);
+    }
+}
