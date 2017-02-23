8c52e5f2163ed3e22d828484c5bcae40d58e042a
iAcn <iacnlyx@gmail.com>
2017-02-23 18:08:14 +0800

    IconViewActivity：外部点击关闭

--- app/src/main/java/me/iacn/mbestyle/ui/activity/IconViewActivity.java
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/IconViewActivity.java
@@ -3,6 +3,7 @@ package me.iacn.mbestyle.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import me.iacn.mbestyle.R;
 
@@ -11,11 +12,22 @@ import me.iacn.mbestyle.R;
  * Emali iAcn0301@foxmail.com
  */
 
-public class IconViewActivity extends AppCompatActivity {
+public class IconViewActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icon_view);
+
+        findViewById(R.id.fl_container).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fl_container:
+                onBackPressed();
+                break;
+        }
     }
 }
