2b542ef02af2e84c8503b5c63e5e145021abd093
iAcn <iacnlyx@gmail.com>
2017-03-12 13:11:21 +0800

    LeanApi：请求 Json 生成

--- app/src/main/java/me/iacn/mbestyle/network/LeanApi.java
+++ app/src/main/java/me/iacn/mbestyle/network/LeanApi.java
@@ -1,14 +1,20 @@
 package me.iacn.mbestyle.network;
 
+import android.text.TextUtils;
+
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import io.reactivex.Observable;
 import me.iacn.mbestyle.BuildConfig;
-import me.iacn.mbestyle.bean.LeanBean;
+import me.iacn.mbestyle.bean.RequestBean;
+import me.iacn.mbestyle.bean.leancloud.LeanBatchRequest;
+import me.iacn.mbestyle.bean.leancloud.LeanQueryBean;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -65,10 +71,50 @@ public class LeanApi {
                 .create(LeanService.class);
     }
 
-    public Observable<LeanBean> queryRequestTotal(String packageName) {
+    public Observable<LeanQueryBean> queryRequestTotal(String packageName) {
         Map<String, String> where = new HashMap<>();
         where.put("packageName", packageName);
 
         return mLeanService.queryRequestTotal(new Gson().toJson(where));
+    }
+
+    public Observable<List<Boolean>> postRequests(List<RequestBean> list) {
+        Map<String, String> map = new HashMap<>();
+        Gson gson = new Gson();
+        StringBuilder builder = new StringBuilder();
+
+        LeanBatchRequest request = new LeanBatchRequest();
+        request.requests = new ArrayList<>();
+
+        for (RequestBean bean : list) {
+            LeanBatchRequest.RequestsBean req = new LeanBatchRequest.RequestsBean();
+
+            if (TextUtils.isEmpty(bean.objectId)) {
+                // 新建 object
+
+                map.put("appName", bean.name);
+                map.put("packageName", bean.packageName);
+                map.put("componentInfo", bean.activity);
+
+                req.method = LeanBatchRequest.METHOD_POST;
+                req.path = "/1.1/classes/AppReport/";
+                req.body = gson.toJson(map);
+
+                map.clear();
+
+            } else {
+                // 更新+1 requestTotal
+
+                req.method = LeanBatchRequest.METHOD_PUT;
+                req.path = builder.append("/1.1/classes/AppReport/").append(bean.objectId).toString();
+                req.body = "{\"requestTotal\":{\"__op\":\"Increment\",\"amount\":1}}";
+
+                builder.delete(0, builder.length());
+            }
+        }
+
+        String str = gson.toJson(request);
+        System.out.println(str);
+        return null;
     }
 }
