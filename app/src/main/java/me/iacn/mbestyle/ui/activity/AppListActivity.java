072bbe7f5bc9802be4860049f6ceece9bb7b7179
meolunr <meolunr@gmail.com>
2017-03-13 13:53:25 +0800

    AppListActivity：建立

---
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/AppListActivity.java
@@ -0,0 +1,17 @@
+package me.iacn.mbestyle.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by iAcn on 2017/3/13
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class AppListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
