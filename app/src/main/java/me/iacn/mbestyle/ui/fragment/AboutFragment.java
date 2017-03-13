925158c55698ca275c6149d17072ab635b954ba0
iAcn <iacnlyx@gmail.com>
2017-03-13 16:56:47 +0800

    AppListActivity：删删删不做了！

--- app/src/main/java/me/iacn/mbestyle/ui/fragment/AboutFragment.java
+++ app/src/main/java/me/iacn/mbestyle/ui/fragment/AboutFragment.java
@@ -6,7 +6,6 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.SystemClock;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,7 +15,6 @@ import com.bumptech.glide.Glide;
 import de.psdev.licensesdialog.LicensesDialog;
 import me.iacn.mbestyle.BuildConfig;
 import me.iacn.mbestyle.R;
-import me.iacn.mbestyle.ui.activity.AppListActivity;
 import me.iacn.mbestyle.ui.widget.AboutItem;
 import moe.feng.alipay.zerosdk.AlipayZeroSdk;
 
@@ -31,10 +29,7 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
     private AboutItem aiVersion;
     private AboutItem aiDesigner;
     private AboutItem aiDeveloper;
-    private AboutItem aiDonate;
     private AboutItem aiOpenSource;
-
-    private long[] mHits = new long[3];
 
     @Override
     protected int getContentView() {
@@ -47,7 +42,6 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
         aiVersion = (AboutItem) findViewById(R.id.ai_version);
         aiDesigner = (AboutItem) findViewById(R.id.ai_designer);
         aiDeveloper = (AboutItem) findViewById(R.id.ai_developer);
-        aiDonate = (AboutItem) findViewById(R.id.ai_donate);
         aiOpenSource = (AboutItem) findViewById(R.id.ai_open_source);
     }
 
@@ -56,7 +50,6 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
         aiVersion.setOnClickListener(this);
         aiDesigner.setOnClickListener(this);
         aiDeveloper.setOnClickListener(this);
-        aiDonate.setOnClickListener(this);
         aiOpenSource.setOnClickListener(this);
     }
 
@@ -69,9 +62,6 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.ai_version:
-                openAppList();
-                break;
             case R.id.ai_designer:
                 openUrl("http://coolapk.com/u/433446");
                 break;
@@ -84,17 +74,6 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
             case R.id.ai_open_source:
                 showLicenseDialog();
                 break;
-        }
-    }
-
-    private void openAppList() {
-        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
-        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
-
-        if (mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
-            // TODO:打开应用列表
-            Intent intent = new Intent(getActivity(), AppListActivity.class);
-            startActivity(intent);
         }
     }
 
