99c80ddafa6ba1108afcfb782c72c94222b1a24b
iAcn <iacnlyx@gmail.com>
2017-05-02 15:17:39 +0800

    抽取 Toolbar Layout

---
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/WallpaperActivity.java
@@ -0,0 +1,11 @@
+package me.iacn.mbestyle.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by iAcn on 2017/5/2
+ * Emali iAcn0301@foxmail.com
+ */
+
+public class WallpaperActivity extends AppCompatActivity {
+}
