dd78f3ff77e526699284203d05aaee6e1d84a738
iAcn <iacnlyx@gmail.com>
2017-06-23 22:20:36 +0800

    Main.py: 缩放 Drawable 至 Miui 的尺寸

--- miui/main.py
+++ miui/main.py
@@ -1,8 +1,12 @@
 import os
 import shutil
 import xml.etree.ElementTree as ET
+import zipfile
+
+from PIL import Image
 
 res_folder = '../app/src/main/res'
+miui_zoom_multiples = 0.75
 
 
 def parse_xml():
@@ -30,7 +34,7 @@ def parse_xml():
 
 
 def move_drawable_to_temp(map):
-    print('>>> 复制 Drawable 至临时文件夹...')
+    print('>>> 复制 Drawable 至临时文件夹...\n')
 
     if os.path.exists('icons_temp'):
         shutil.rmtree('icons_temp')
@@ -44,8 +48,38 @@ def move_drawable_to_temp(map):
         shutil.copyfile(src_path, out_path)
 
 
+def zoom_for_miui():
+    print('>>> 缩放 Drawable 到 Miui 的尺寸...\n')
+
+    for parent, dirs, files in os.walk('icons_temp'):
+        for file in files:
+            print('>>> 正在转换 %s' % file)
+
+            file_path = os.path.join('icons_temp', file)
+            src_img = Image.open(file_path)
+
+            converted_x = int(src_img.size[0] * miui_zoom_multiples)
+            converted_y = int(src_img.size[1] * miui_zoom_multiples)
+
+            offset_x = (src_img.size[0] - converted_x) / 2
+            offset_y = (src_img.size[1] - converted_y) / 2
+
+            resize_img = src_img.resize((converted_x, converted_y), Image.ANTIALIAS)
+
+            out_img = Image.new('RGBA', src_img.size, 255)
+            # 这里将 box 内的所有数据转为 int
+            box = tuple(map(int, (offset_x, offset_y, converted_x + offset_x, converted_y + offset_y)))
+            out_img.paste(resize_img, box)
+            src_img.close()
+
+            out_img.save(file_path)
+            out_img.close()
+
+
 if __name__ == '__main__':
     print('>>> 开始运行\n')
 
     icon_map = parse_xml()
     move_drawable_to_temp(icon_map)
+    zoom_for_miui()
+   
