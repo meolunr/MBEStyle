7bdf21339410d42beb433ea6e46e7d3a0c3e259f
meolunr <meolunr@gmail.com>
2019-04-01 20:31:54 +0800

    Fixed a crash when set wallpaper

--- app/src/main/java/me/iacn/mbestyle/ui/activity/WallpaperActivity.java
+++ app/src/main/java/me/iacn/mbestyle/ui/activity/WallpaperActivity.java
@@ -1,10 +1,17 @@
 package me.iacn.mbestyle.ui.activity;
 
+import android.Manifest;
 import android.app.WallpaperManager;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.annotation.RawRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -31,7 +38,6 @@ import me.iacn.mbestyle.util.StatusBarUtils;
 
 public class WallpaperActivity extends AppCompatActivity {
 
-    private RecyclerView rvWallpaper;
     private int[] mIds;
 
     @Override
@@ -40,12 +46,12 @@ public class WallpaperActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wallpaper);
         StatusBarUtils.setColor(this, ContextCompat.getColor(this, R.color.colorPrimary));
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("请选择一张壁纸");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        rvWallpaper = (RecyclerView) findViewById(R.id.rv_wallpaper);
+        RecyclerView rvWallpaper = findViewById(R.id.rv_wallpaper);
         rvWallpaper.setLayoutManager(new GridLayoutManager(this, 2));
 
         mIds = new int[]{
@@ -62,42 +68,19 @@ public class WallpaperActivity extends AppCompatActivity {
         adapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(View itemView, int position) {
-                File file;
-
-                try {
-                    InputStream inputStream = getResources().openRawResource(mIds[position]);
-                    byte[] bytes = new byte[inputStream.available()];
-                    int len = inputStream.read(bytes);
-
-                    // 权限问题，输出到外置目录
-                    file = new File(getExternalFilesDir(""), getResources().getResourceEntryName(mIds[position]));
-                    FileOutputStream outputStream = new FileOutputStream(file);
-                    outputStream.write(bytes, 0, len);
-
-                    inputStream.close();
-                    outputStream.close();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    showFailedToast();
-                    return;
-                }
-
-                Uri uri = Uri.fromFile(file);
-
-                try {
-                    WallpaperManager manager = WallpaperManager.getInstance(WallpaperActivity.this);
-                    startActivity(manager.getCropAndSetWallpaperIntent(uri));
-
-                } catch (Exception e) {
-                    Intent intent = new Intent(Intent.ACTION_ATTACH_DATA);
-                    intent.setDataAndType(uri, "image/*");
-                    intent.putExtra("mimeType", "image/*");
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(Intent.createChooser(intent, "设置壁纸"));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        setWallpaper(mIds[position]);
+                    }
+                } else {
+                    setWallpaper(mIds[position]);
                 }
             }
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
     }
 
     @Override
@@ -108,5 +91,68 @@ public class WallpaperActivity extends AppCompatActivity {
 
     private void showFailedToast() {
         Toast.makeText(this, "壁纸设置失败", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setWallpaper(@RawRes int rawId) {
+        File imageFile;
+
+        try {
+            InputStream inputStream = getResources().openRawResource(rawId);
+            byte[] bytes = new byte[inputStream.available()];
+            int len = inputStream.read(bytes);
+
+            // 权限问题，输出到外置目录
+            imageFile = new File(getExternalFilesDir(""), getResources().getResourceEntryName(rawId));
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            outputStream.write(bytes, 0, len);
+
+            inputStream.close();
+            outputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            showFailedToast();
+            return;
+        }
+
+        Uri uri = getImageContentUri(imageFile);
+
+        try {
+            WallpaperManager manager = WallpaperManager.getInstance(WallpaperActivity.this);
+            startActivity(manager.getCropAndSetWallpaperIntent(uri));
+
+        } catch (Exception e) {
+            Intent intent = new Intent(Intent.ACTION_ATTACH_DATA);
+            intent.setDataAndType(uri, "image/*");
+            intent.putExtra("mimeType", "image/*");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(Intent.createChooser(intent, "设置壁纸"));
+        }
+    }
+
+    private Uri getImageContentUri(File imageFile) {
+        String filePath = imageFile.getAbsolutePath();
+        Cursor cursor = getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[]{MediaStore.Images.Media._ID},
+                MediaStore.Images.Media.DATA + "=? ",
+                new String[]{filePath}, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor
+                    .getColumnIndex(MediaStore.MediaColumns._ID));
+            cursor.close();
+            Uri baseUri = Uri.parse("content://media/external/images/media");
+            return Uri.withAppendedPath(baseUri, "" + id);
+        } else {
+            if (imageFile.exists()) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, filePath);
+                return getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } else {
+                return null;
+            }
+        }
     }
 }
